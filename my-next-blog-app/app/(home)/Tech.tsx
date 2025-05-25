6b608f1f00e6e5e23e6d7c329a2c259c9e834b87
tunkrawaysoe <57566289+tunkrawaysoe@users.noreply.github.com>
2025-05-25 12:34:35 +0630

    feat: add reusable Card component with dynamic layout and image support

--- my-next-blog-app/app/(home)/Tech.tsx
+++ my-next-blog-app/app/(home)/Tech.tsx
@@ -1,10 +1,39 @@
 import React from 'react'
+import Card from '../(shared)/Card'
+
 
 type Props = {}
 
 const Tech = (props: Props) => {
   return (
-    <section className='py-4'>Tech</section>
+    <section className='py-4'>
+      <div className="sm:grid grid-cols-2 grid-rows-3  gap-4">
+              {/* LARGE CARD */}
+            <Card
+              className="col-span-1 row-span-3"
+              imageHeight="h-96"
+              isLongForm
+            />
+            {/* SMALL CARDS */}
+            <Card
+              className="col-span-1 row-span-1 mt-10 sm:mt-0 flex justify-between gap-3"
+              imageHeight="h-48"
+              
+              isSmallCard
+            />
+            <Card
+              className="col-span-1 row-span-1 mt-10 sm:mt-0 flex justify-between gap-3"
+              imageHeight="h-48"
+            
+              isSmallCard
+            />
+            <Card
+              className="col-span-1 row-span-1 mt-10 sm:mt-0 flex justify-between gap-3"
+              imageHeight="h-48"
+              isSmallCard
+            />
+          </div>
+    </section>
   )
 }
 
---
+++ my-next-blog-app/app/(shared)/Card.tsx
@@ -0,0 +1,62 @@
+import Link from 'next/link';
+import React from 'react'
+import ai4 from '../../public/assets/ai-4.jpg'
+import Image from 'next/image';
+
+type Props = {
+  className? : string ;
+  isLongForm? : boolean;
+  isSmallCard? : boolean;
+  imageHeight? :string;
+}
+
+const Card = ({className,isLongForm,isSmallCard,imageHeight}: Props) => {
+  return (
+    <div className={className}>
+      <Link
+        className="basis-full hover:opacity-70"
+        href={`/`}
+      >
+        <div className={`relative mb-3 ${imageHeight}`}>
+          <Image
+            fill
+            alt="tech"
+            placeholder="blur"
+            src={ai4}
+            style={{ objectFit: "cover" }}
+          />
+        </div>
+      </Link>
+      <div className="basis-full">
+        <Link href={`/`}>
+          <h4
+            className={`font-bold hover:text-accent-green
+            ${isSmallCard ? "text-base" : "text-lg"}
+            ${isSmallCard ? "line-clamp-2" : ""}
+          `}
+          >
+            eoifnudfnudufnun
+          
+          </h4>
+        </Link>
+
+        <div className={`${isSmallCard ? "my-2" : "flex my-3"} gap-3`}>
+          <h5 className="font-semibold text-xs">fgnoinonpifoigofi</h5>
+          <h6 className="text-wh-300 text-xs">what the fuck are </h6>
+        </div>
+        <p
+          className={`text-wh-500 ${
+            isLongForm ? "line-clamp-5" : "line-clamp-3"
+          }`}
+        >
+          yoidnvundufiu
+        
+        </p>
+      </div>
+    </div>
+  );
+
+  
+}
+
+export default Card
